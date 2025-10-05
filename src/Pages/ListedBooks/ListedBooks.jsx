1789f072c43cbf7f5855a5eab1ae1b89acf26edd
hasnatfahmidkhan <hasnatfahmidkhan@gmail.com>
2025-10-05 17:34:42 +0600

    added sort features

--- src/Pages/ListedBooks/ListedBooks.jsx
+++ src/Pages/ListedBooks/ListedBooks.jsx
@@ -6,10 +6,13 @@ import { getStoredData } from "../../Utility/addToDB";
 import ReadListCard from "../../Components/ReadListCard/ReadListCard";
 import WishListCard from "../../Components/WishListCard/WishListCard";
 import { getWishListData } from "../../Utility/addWishListToDB";
+import { ChevronDown } from "lucide-react";
 const ListedBooks = () => {
   const { data: books } = useLoaderData();
   const [storedBooks, setstoredBooks] = useState([]);
   const [storedWishLists, setStoredWishLists] = useState([]);
+  const [arrow, setArrow] = useState(false);
+  const [selected, setSelected] = useState("Sort By");
 
   useEffect(() => {
     const booksId = getStoredData();
@@ -31,10 +34,71 @@ const ListedBooks = () => {
     setStoredWishLists(wishLists);
   }, [books]);
 
+  const handleSelect = (option) => {
+    setSelected(option);
+    setArrow(false); // for close the dropdown content
+
+    if (option === "Rating") {
+      const sortedReadBooks = [...storedBooks].sort(
+        (a, b) => b.rating - a.rating
+      );
+      setstoredBooks(sortedReadBooks);
+      setStoredWishLists(sortedReadBooks);
+    }
+
+    if (option === "Number of pages") {
+      const sortedReadBooks = [...storedBooks].sort(
+        (a, b) => a.rating - b.rating
+      );
+      setstoredBooks(sortedReadBooks);
+      setStoredWishLists(sortedReadBooks);
+    }
+    if (option === "Publisher year") {
+      const sortedReadBooks = [...storedBooks].sort(
+        (a, b) => a.rating - b.rating
+      );
+      setstoredBooks(sortedReadBooks);
+      setStoredWishLists(sortedReadBooks);
+    }
+  };
   return (
     <section>
       <div className="bg-[#F3F3F3] text-center py-10 rounded-xl">
         <h2 className="text-4xl font-semibold">Books</h2>
+      </div>
+
+      <div className="relative w-52 mt-10 mx-auto">
+        <div
+          onClick={() => setArrow(!arrow)}
+          tabIndex={0}
+          role="button"
+          className="btn m-1 bg-[#23BE0A] text-white w-full text-base py-6 rounded-md flex items-center justify-center"
+        >
+          {selected} <ChevronDown />
+        </div>
+
+        {arrow && (
+          <ul className="absolute top-full left-0 mt-2 menu bg-base-100 rounded-md z-10 w-full text-center p-2 shadow-md space-y-2 text-lg text-gray-700 cursor-pointer">
+            <li
+              className="hover:bg-gray-200 py-2 rounded"
+              onClick={() => handleSelect("Rating")}
+            >
+              Rating
+            </li>
+            <li
+              className="hover:bg-gray-200 py-2 rounded"
+              onClick={() => handleSelect("Number of pages")}
+            >
+              Number of pages
+            </li>
+            <li
+              className="hover:bg-gray-200 py-2 rounded"
+              onClick={() => handleSelect("Publisher year")}
+            >
+              Publisher year
+            </li>
+          </ul>
+        )}
       </div>
       <div className="py-10">
         <Tabs>
